b026722a606c641ffb1a2382ce842937a488f378
Soh Hong Yu <sohhongyu@gmail.com>
2025-05-05 18:44:07 +0800

    Update Scanner Page using NextJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,103 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useCallback } from "react";
+import {
+  Scanner as ScannerComp,
+  IScannerProps,
+  centerText,
+} from "@yudiel/react-qr-scanner";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Menu } from "lucide-react";
+
+export default function ScannerPage(props: Partial<IScannerProps>) {
+  const handleScan = useCallback((detectedCodes: any) => {
+    console.log("Scanned:", detectedCodes);
+    toast.success(`Scanned: ${detectedCodes[0].rawValue}`);
+  }, []);
+
+  const handleError = useCallback((error: unknown) => {
+    if (error instanceof Error) {
+      console.error("Scanner error:", error.message);
+      toast.error(`Scanner error: ${error.message}`);
+    } else {
+      console.error("Unknown scanner error:", error);
+      toast.error(`Unknown scanner error:: ${error}`);
+    }
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="flex items-center fullHeight">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              className="fixed top-2.5 left-2.5"
+              size={"icon"}
+              variant={"secondary"}
+            >
+              <Menu />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="w-4/5 mx-auto aspect-square max-w-3xl">
+          <ScannerComp
+            {...props}
+            formats={[
+              "qr_code",
+              "micro_qr_code",
+              "rm_qr_code",
+              "maxi_code",
+              "pdf417",
+              "aztec",
+              "data_matrix",
+              "matrix_codes",
+              "dx_film_edge",
+              "databar",
+              "databar_expanded",
+              "codabar",
+              "code_39",
+              "code_93",
+              "code_128",
+              "ean_8",
+              "ean_13",
+              "itf",
+              "linear_codes",
+              "upc_a",
+              "upc_e",
+            ]}
+            onScan={handleScan}
+            onError={handleError}
+            components={{
+              onOff: false,
+              torch: true,
+              zoom: true,
+              finder: true,
+              tracker: centerText,
+            }}
+            allowMultiple={false}
+            scanDelay={2000}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
